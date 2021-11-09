ff3a09a17dbba12b045987d13f095ce9cef8db99
莊理竣 <lijungjhuang@uitc.com.tw>
2021-11-09 17:33:52 +0800

    新增ReponseModel

---
+++ GodPay-CMS/Common/Enums/ReturnCodeEnum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GodPay_CMS.Common.Enums
+{
+    /// <summary>
+    /// 回傳Ajax狀態碼
+    /// </summary>
+    public enum ReturnCodeEnum
+    {
+        /// <summary>回應失敗</summary>
+        [Description("失敗")]
+        Fail = 0,
+
+        /// <summary>回應成功</summary>
+        [Description("成功")]
+        Success = 1,
+    }
+}
---
+++ GodPay-CMS/Controllers/ViewModels/ResponseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using GodPay_CMS.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GodPay_CMS.Controllers.ViewModels
+{
+    /// <summary>
+    /// 回傳Ajax Model
+    /// </summary>
+    /// <typeparam name="T">回傳資料型態</typeparam>
+    public class ResponseViewModel<T>
+    {
+        /// <summary>
+        /// 回傳狀態碼
+        /// </summary>
+        public ReturnCodeEnum RtnCode { get; set; }
+        /// <summary>
+        /// 回傳狀態訊息
+        /// </summary>
+        public string RtnMessage { get; set; }
+        /// <summary>
+        /// 回傳狀態資料
+        /// </summary>
+        public T RtnData { get; set; }
+    }
+}
