63bf9f3ede51640126b205482640df18ec32f4a0
莊理竣 <lijungjhuang@uitc.com.tw>
2021-11-09 17:03:13 +0800

    [CMS] Login Verify 前端完成

---
+++ GodPay-CMS/Controllers/SigninOperateController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using GodPay_CMS.Controllers.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GodPay_CMS.Controllers
+{
+    public class SigninOperateController : Controller
+    {
+        private readonly IMapper mapper;
+        public SigninOperateController(IMapper Mapper)
+        {
+            mapper = Mapper;
+        }
+
+        [HttpPost]
+        public IActionResult SignIn([FromBody] UserViewModel UserViewModel)
+        {
+            return Ok(UserViewModel);
+        }
+    }
+}
---
+++ GodPay-CMS/StaticFiles/Vue/Methods/Common.js
@@ -0,0 +1,9 @@
+﻿export function CheckValueOfObjectIsNotEmpty(values) {
+    console.log(values)
+    for (let key in values) {
+        if (values[key] === '') {
+            return false;
+        }
+    }
+    return true;
+}
