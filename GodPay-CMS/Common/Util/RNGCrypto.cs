6b15bc74db80dec790fd58f2c5ee27d90f10049f
Michelle <michellechen@uitc.com.tw>
2021-11-10 16:36:08 +0800

    新增RNGCrypto

---
+++ GodPay-CMS/Common/Util/RNGCrypto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace GodPay_CMS.Common.Util
+{
+    public static class RNGCrypto
+    {
+        public static string HMACSHA256(string message, string key)
+        {
+            var encoding = new System.Text.UTF8Encoding();
+            byte[] keyByte = encoding.GetBytes(key);
+            byte[] messageBytes = encoding.GetBytes(message);
+            using (var hmacSHA256 = new HMACSHA256(keyByte))
+            {
+                byte[] hashMessage = hmacSHA256.ComputeHash(messageBytes);
+                return BitConverter.ToString(hashMessage).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
