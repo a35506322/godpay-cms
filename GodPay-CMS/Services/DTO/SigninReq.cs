350ae2ef68e9c5222f491ad821634788f4e25126
Michelle <michellechen@uitc.com.tw>
2021-11-10 15:20:09 +0800

    新增SigninReq、SigninService

---
+++ GodPay-CMS/Services/DTO/SigninReq.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GodPay_CMS.Services.DTO
+{
+    public class SigninReq
+    {
+        public string UserId { get; set; }
+        public string UserKey { get; set; }
+    }
+}
---
+++ GodPay-CMS/Services/Implements/SigninService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GodPay_CMS.Services.Implements
+{
+    public class SigninService
+    {
+    }
+}
