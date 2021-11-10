c9fe09bf6ef7d17655281a2181e63b6d27eac62c
Michelle <michellechen@uitc.com.tw>
2021-11-10 15:19:23 +0800

    新增SigninMapper

---
+++ GodPay-CMS/Common/Profiles/SigninMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using GodPay_CMS.Controllers.ViewModels;
+using GodPay_CMS.Services.DTO;
+
+namespace GodPay_CMS.Common.Profiles
+{
+    public class SigninMapper : Profile
+    {
+        public SigninMapper()
+        {
+            CreateMap<SigninViewModel, SigninReq>();
+        }
+    }
+}
