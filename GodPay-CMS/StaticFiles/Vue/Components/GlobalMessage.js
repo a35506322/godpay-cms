84aa2053b1be517e5d1f85f3e46f39e91f0e1984
莊理竣 <lijungjhuang@uitc.com.tw>
2021-11-16 17:49:13 +0800

    新增推波元件

---
+++ GodPay-CMS/StaticFiles/Vue/Components/GlobalMessage.js
@@ -0,0 +1,34 @@
+﻿export default {
+    props: ['title', 'status', 'message'],
+    data: function () {
+        return {
+            toast: {}
+        }
+    },
+    mounted: function () {
+        const option = {
+            delay : 10000
+        }
+        this.toast = new bootstrap.Toast(this.$refs.globalMessage, option);
+        this.show();
+    },
+    methods: {
+        show: function () {
+            this.toast.show();
+        }
+    },
+    template:
+        `
+            <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" ref="globalMessage">
+                <div class="toast-header">
+                    <span v-bind:class="'bg-'+status" class="p-2 rounded me-2 d-inline-block"></span>
+                    <strong class="me-auto">{{title}}</strong>
+                    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                </div>
+                <div class="toast-body">
+                    {{message}}
+                </div>
+            </div>
+        `
+
+}
---
+++ GodPay-CMS/StaticFiles/Vue/Components/MessagePush.js
@@ -0,0 +1,39 @@
+﻿import globalMessage from '/StaticFiles/Vue/Components/GlobalMessage.js'
+export default {
+    components: {
+        globalMessage
+    },
+    data: function () {
+        return {
+            globalMessages:[],
+        }
+    },
+    mounted: function () {
+        this.$emitter.on('GlobalMessage', request => {
+            let { title = '', status = 'success', message = '' } = request;
+            this.globalMessages.push({ title, status, message });
+        })
+    },
+    watch: {
+        globalMessages: {
+            handler: function (newVal, oldVal) {
+                if (newVal.length > 9) {
+                    this.globalMessages = []
+                }
+            },
+            // 物件時候需要深度監聽
+            deep: true
+        }
+    },
+    template:
+        `
+        <template v-if="globalMessages.length>0">
+            <div class="toast-container position-absolute top-0 end-0 p-3 z-index-1030">
+                <template v-for="(message,index) in globalMessages" v-bind:key="'key'+index">                
+                        <global-message v-bind="message"></global-message>                         
+                </template>
+            </div>
+        </template>
+        `
+
+}
