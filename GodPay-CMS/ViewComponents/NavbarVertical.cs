fa7ed8c01be03b14838668805e0286966bd6f898
莊理竣 <lijungjhuang@uitc.com.tw>
2021-11-18 14:35:24 +0800

    調整回傳型態

--- GodPay-CMS/ViewComponents/NavbarVertical.cs
+++ GodPay-CMS/ViewComponents/NavbarVertical.cs
@@ -1,5 +1,7 @@
 ﻿using GodPay_CMS.Common.Enums;
 using GodPay_CMS.Controllers.ViewModels;
+using GodPay_CMS.Services.DTO;
+using GodPay_CMS.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,73 +14,15 @@ namespace GodPay_CMS.ViewComponents
 {
     public class NavbarVertical : ViewComponent
     {
-        private List<AuthorityViewModel> authorityViewModels { get; set; }
         private readonly IHttpContextAccessor _httpContextAccessor;
-        public NavbarVertical(IHttpContextAccessor httpContextAccessor)
+        private readonly IAuthorityService _authorityService;
+        public NavbarVertical(IHttpContextAccessor httpContextAccessor, IAuthorityService authorityService)
         {
             _httpContextAccessor = httpContextAccessor;
-
-            authorityViewModels = new List<AuthorityViewModel>();
-
-            authorityViewModels.Add
-            (
-               new AuthorityViewModel()
-               {
-                   FuncClassEnName = "home",
-                   FuncClassChName = "首頁",
-                   Functions = new List<FunctionViewModel>()
-                   {
-                        new FunctionViewModel ()
-                        {
-                            FuncEnName ="index",
-                            FuncChName ="控制台"
-                        }
-                   }
-               }
-            );
-
-            authorityViewModels.Add
-            (
-                new AuthorityViewModel()
-                {
-                    FuncClassEnName = "profile",
-                    FuncClassChName = "帳號管理",
-                    Functions = new List<FunctionViewModel>()
-                    {
-                        new FunctionViewModel ()
-                        {
-                             FuncEnName ="edituser",
-                             FuncChName ="編輯使用者"
-                        },
-                        new FunctionViewModel ()
-                        {
-                             FuncEnName ="changekey",
-                             FuncChName ="變更密碼"
-                        }
-                    }
-                }
-            );
-
-            authorityViewModels.Add
-            (
-                new AuthorityViewModel()
-                {
-                    FuncClassEnName = "system",
-                    FuncClassChName = "系統功能設定",
-                    Functions = new List<FunctionViewModel>()
-                    {
-                        new FunctionViewModel ()
-                        {
-                             FuncEnName ="functionsetting",
-                             FuncChName ="功能設定"
-                        }
-                    }
-                }
-            );
-
+            _authorityService = authorityService;
         }
 
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
             var role = _httpContextAccessor.HttpContext.User.Claims.SingleOrDefault(claim => claim.Type == ClaimTypes.Role).Value;
             if (role != RoleEnum.Admin.ToString())
@@ -87,7 +31,8 @@ namespace GodPay_CMS.ViewComponents
             }
             else
             {
-                return View("NavbarVertical", authorityViewModels);
+                var response = await _authorityService.GetListOfFunctions();
+                return View("NavbarVertical", response.RtnData as IEnumerable<FunctionListViewModel>);
             }
         }
     }
