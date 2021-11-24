33151f212e231d619d5ba659e8badf9da7dca822
莊理竣 <lijungjhuang@uitc.com.tw>
2021-11-24 17:44:23 +0800

    新增查詢業務詳細資料頁面

---
+++ GodPay-CMS/StaticFiles/Vue/Components/BusinessmanDetailModal.js
@@ -0,0 +1,60 @@
+﻿export default {
+    props: ['pdetail'],
+    data: function () {
+        return {
+            displayModal: false,
+            detail: {}
+        }
+    },
+    methods: {
+        Show: function () {
+            this.displayModal = true;
+        },
+        Close: function () {
+            this.displayModal = false;
+        }
+    },
+    watch: {
+        pdetail: {
+            handler(newVal, oldVal) {
+                this.detail = { ...this.pdetail };
+            },
+            deep: true
+        }
+    },
+    template: `
+    <p-dialog header="業務詳細資料" v-model:visible="displayModal" v-bind:style="{width:'50vw'}" v-bind:position="'top'" v-bind:modal="true">
+         <div class="modal-body">
+            <div class="container-fluid">
+              <div class="p-datatable p-component p-datatable-responsive-scroll">
+                    <div class="p-datatable-wrapper">
+                      <table class="p-datatable-table">
+                            <tbody class="p-datatable-tbody">
+                                <tr>
+                                    <td>
+                                        <div class="row">
+                                            <label class="col-sm-2 col-form-label">姓名</label>
+                                            <div class="col-sm-10">
+                                                <input type="text" readonly class="form-control-plaintext outline-none" v-bind:value="detail.UserId">
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="row">
+                                            <label class="col-sm-2 col-form-label">部門</label>
+                                            <div class="col-sm-10">
+                                                <input type="text" readonly class="form-control-plaintext outline-none" v-bind:value="detail.Department">
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+         </div>
+    </p-dialog>`
+}
