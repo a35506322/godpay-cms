0fe90e25f8e770aef5bd73d3ff6467cb66bfcad7
莊理竣 <lijungjhuang@uitc.com.tw>
2021-11-25 10:49:18 +0800

    Axios Service針對200代碼以外處理統一化

--- GodPay-CMS/StaticFiles/Vue/Services/Axios.js
+++ GodPay-CMS/StaticFiles/Vue/Services/Axios.js
@@ -4,18 +4,25 @@ axios.defaults.baseURL = window.baseURL;
 axios.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
 
 // Error Handle
-const errorHandle = (status) => {
+const errorHandle = (response, status) => {
+    // vue
+    const vue = app.__vue_app__.config;
+    let deatil = ''
+    if (typeof (response.data) === 'string') {
+        deatil = response.data;
+    }
     switch (status) {
         case 400:
-            console.log("400")
-            alert('Bad Request');
+            vue.globalProperties.$toast.add({ severity: 'error', summary: 'Bad Request', detail: deatil, life: 5000, group: 'backend-laoout' });
             break;
         case 401:
-            console.log("401")
-            alert('認證失敗');
+            vue.globalProperties.$toast.add({ severity: 'error', summary: '認證失敗', detail: deatil, life: 5000, group: 'backend-laoout' });
+            break;
+        case 500:       
+            vue.globalProperties.$toast.add({ severity: 'error', summary: '伺服器內部錯誤', detail: deatil, life: 5000, group:'backend-laoout'});
             break;
         default:
-            alert('伺服器發生內部問題');
+            vue.globalProperties.$toast.add({ severity: 'error', summary: '意外狀況', detail: '請與資訊部聯絡', life: 5000, group: 'backend-laoout' });
             break;
     }
 }
@@ -35,22 +42,29 @@ axios.interceptors.response.use(
         return response;
     },
     (error) => {
-        console.log('error', response);
-        console.log('error', error);
-        const { response } = error;
+        // vue
+        const vue = app.__vue_app__.config;
+        const { response } = error;       
         if (response) {
-            // 成功發出請求且收到 response, 但有 error
-            errorHandle(response.status);
-            return Promise.reject(error);
+            // 按照狀態發出訊息
+            errorHandle(response,response.status);
+            // 成功發出請求且收到 response
+            console.log('error', response);
+            return Promise.reject(response);
         } else {
             // 成功發出請求但沒收到 response
             if (!window.navigator.onLine) {
                 //如果是網路斷線
-                alert('網路出了問題, 請重新連線');
+                vue.globalProperties.$toast.add({ severity: 'error', summary: '網路出了問題, 請重新連線', detail: '', life: 5000, group: 'backend-laoout' });
             } else {
                 // 其它問題 
-                alert('主機伺服器發生問題, 請連絡資訊單位');
-                return Promise.reject(error);
+                vue.globalProperties.$toast.add({ severity: 'error', summary: '主機伺服器發生問題, 請連絡資訊單位', detail: '', life: 5000, group: 'backend-laoout' });
+                let errorResponse = {
+                    message: error.message,
+                    request: error.request
+                }
+                console.log('errorResponse',errorResponse);
+                return Promise.reject(errorResponse);
             }
         }
     }
