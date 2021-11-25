92cbad31d0314fd9f0362fbf7d9e6ac9e26469d9
莊理竣 <lijungjhuang@uitc.com.tw>
2021-11-25 15:54:21 +0800

    新增註解

--- GodPay-CMS/Repositories/Entity/Insider.cs
+++ GodPay-CMS/Repositories/Entity/Insider.cs
@@ -5,11 +5,26 @@ using System.Threading.Tasks;
 
 namespace GodPay_CMS.Repositories.Entity
 {
+    /// <summary>
+    /// 內部人員詳細資料
+    /// </summary>
     public class Insider
     {
+        /// <summary>
+        /// 流水號(PK)
+        /// </summary>
         public int Iid { get; set; }
+        /// <summary>
+        /// 帳號
+        /// </summary>
         public string UserId { get; set; }
+        /// <summary>
+        /// 名子
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// 部門
+        /// </summary>
         public string Department { get; set; }
     }
 }
