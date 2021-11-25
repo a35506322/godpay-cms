96cdfbaa816bb0b8ab4d0cccd7ddc20ce66a03d8
莊理竣 <lijungjhuang@uitc.com.tw>
2021-11-25 10:51:47 +0800

    1.統一在Service回傳資料 2.改為非同步模式

--- GodPay-CMS/StaticFiles/Vue/Services/IpassService.js
+++ GodPay-CMS/StaticFiles/Vue/Services/IpassService.js
@@ -1,5 +1,13 @@
 ﻿import axios from '/StaticFiles/Vue/Services/Axios.js'
 
-export const PostSignin = data => axios.post('/SigninOperate/SignIn', data);
-export const GetBusinessmens = () => axios.get('/BusinessManagementApi/GetBusinessmens');
-export const GetBusinessmanDetail = data => axios.get('/BusinessManagementApi/GetBusinessmenDeatil',data);
+// SigninOperateController
+export const PostSignin = data => axios.post('/SigninOperate/SignIn', data).then(respone => { console.log(' GetBusinessmens'); return respone; }).catch(error => error);
+
+// ProfileApiController
+export const GetUserById = query => axios.get('/ProfileApi/Get', query).then(respone => { console.log('GetUserById'); return respone; }).catch(error => error);
+export const EditUser = data => axios.post('/ProfileApi/Edit', data).then(respone => { console.log('EditUser'); return respone; }).catch(error => error);
+
+
+// BusinessManagementApiController
+export const GetBusinessmens = () => axios.get('/BusinessManagementApi/GetBusinessmens').then(respone => { console.log(' GetBusinessmens'); return respone; }).catch(error => error);
+export const GetBusinessmanDetail = query => axios.get('/BusinessManagementApi/GetBusinessmenDeatil', query).then(respone => { console.log('GetBusinessmanDetail'); return respone; }).catch(error => error);
