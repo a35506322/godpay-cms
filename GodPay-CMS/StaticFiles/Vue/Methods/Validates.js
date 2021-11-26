f615a8ca070bb7ae679bf786875c87d0131cfbd5
莊理竣 <lijungjhuang@uitc.com.tw>
2021-11-26 11:23:37 +0800

    新增前端驗證規則

---
+++ GodPay-CMS/StaticFiles/Vue/Methods/Validates.js
@@ -0,0 +1,5 @@
+﻿export function VerifyAccount  (value) {
+    const regexs = /^(?=.*\d)(?=.*[a-zA-Z]).{6,20}$/;
+    const result = regexs.test(value);
+    return result ? true :'請至少輸入大小寫英文1位、1位數字1位及6-20位帳號'
+}
