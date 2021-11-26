d0590c212d0e18e7fe084fdbdf0093952db12c4f
莊理竣 <lijungjhuang@uitc.com.tw>
2021-11-26 17:48:44 +0800

    新增欄位對應邏輯

--- GodPay-CMS/Common/Profiles/UserProfile.cs
+++ GodPay-CMS/Common/Profiles/UserProfile.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
 using GodPay_CMS.Common.Enums;
 using GodPay_CMS.Common.Helpers;
+using GodPay_CMS.Common.Util;
 using GodPay_CMS.Controllers.ViewModels;
 using GodPay_CMS.Repositories.Entity;
 using GodPay_CMS.Services.DTO;
+using System;
 
 namespace GodPay_CMS.Common.Profiles
 {
@@ -19,11 +21,18 @@ namespace GodPay_CMS.Common.Profiles
 
             CreateMap<EditUserViewModel, UpdateUserReq>();
 
-            CreateMap<PostUserAndInsiderViewModal, PostUserAndInsiderReq>();
+            CreateMap<PostUserAndInsiderViewModel, PostUserAndInsiderReq>()
+                       .ForMember(n=>n.UserKey,o=>o.MapFrom(o=> RNGCrypto.HMACSHA256("p@ssw0rd",o.UserId)))
+                       .ForMember(n=>n.Role,o=>o.MapFrom(t => RoleEnum.Manager))
+                       .ForMember(n => n.CreateDate, o => o.MapFrom(t => DateTime.Now))
+                       .ForMember(n => n.Func, o => o.MapFrom(t => 0));
 
             CreateMap<User, BusinessmanRsp>()
                     .ForMember(n=>n.Name,o=>o.MapFrom(o=>o.Insider.Name))
                     .ForMember(n => n.Department, o => o.MapFrom(o => o.Insider.Department));
+
+            CreateMap<UpdateUserAndInsiderViewModel, UpdateUserAndInsiderReq>()
+                    .ForMember(n => n.LastModifyDate, o => o.MapFrom(t=> DateTime.Now));
         }
     }
 }
