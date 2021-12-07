4f7ff6260642e57eda094270d2dd71b5b4a46876
潘 彥良 <saber30092@gmail.com>
2021-12-07 13:42:01 +0800

    store頁面-編輯功能完成

--- GodPay-CMS/StaticFiles/Vue/Services/IpassService.js
+++ GodPay-CMS/StaticFiles/Vue/Services/IpassService.js
@@ -20,7 +20,8 @@ export const GetBusinessmensFilter = query => axios.get('/BusinessManagementApi/
 export const GetStores = () => axios.get('/StoreManagementApi/GetStores').then(respone => { console.log(' StoreManagementApi'); return respone; }).catch(error => error);
 export const GetStoreDetail = (query) => axios.get('/StoreManagementApi/GetStoreDeatil', query).then(respone => { console.log('GetStoreDeatil'); return respone; }).catch(error => error);
 export const PostStore = data => axios.post('/StoreManagementApi/PostStore', data).then(respone => { console.log('PostStore'); return respone; }).catch(error => error);
-export const GetStore = query => axios.get('/StoreManagementApi/GetStore', query).then(respone => { console.log('PostStore'); return respone; }).catch(error => error);
+export const GetStore = (query) => axios.get('/StoreManagementApi/GetStore', query).then(respone => { console.log('PostStore'); return respone; }).catch(error => error);
+export const UpdateStore = data => axios.post('/StoreManagementApi/UpdateStore', data).then(respone => { console.log('UpdateStore'); return respone; }).catch(error => error);
 
 // EnumApi
 export const GetRoleEnum = () => axios.get('/EnumApi/GetRoleEnum').then(respone => { console.log('GetRoleEnum'); return respone; }).catch(error => error);
