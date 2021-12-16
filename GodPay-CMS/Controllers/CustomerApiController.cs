b615953ba9aaffef39f305d0c66ba9e5332e292e
莊理竣 <lijungjhuang@uitc.com.tw>
2021-12-16 16:08:45 +0800

    新增CustomerController

---
+++ GodPay-CMS/Controllers/CustomerApiController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using GodPay_CMS.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GodPay_CMS.Controllers
+{
+    public class CustomerApiController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly IServiceWrapper _serviceWrapper;
+        public CustomerApiController(IMapper mapper, IServiceWrapper serviceWrapper)
+        {
+            _mapper = mapper;
+            _serviceWrapper = serviceWrapper;
+        }
+    }
+}
