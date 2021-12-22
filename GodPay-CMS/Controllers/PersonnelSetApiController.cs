f6ffcf3e963628843c7084e1826d6485849a7bb6
Michelle <MichelleChen@uitc.com.tw>
2021-12-22 09:52:33 +0800

    加入PersonnelSetController/PersonnelSetApiController

---
+++ GodPay-CMS/Controllers/PersonnelSetApiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GodPay_CMS.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GodPay_CMS.Controllers
+{
+    /// <summary>
+    /// 特店人員管理API
+    /// </summary>
+    [Route("[controller]/[action]")]
+    [Authorize(Roles = "Admin,Manager,Store")]
+    public class PersonnelSetApiController : Controller
+    {
+        private readonly IServiceWrapper _serviceWrapper;
+        public PersonnelSetApiController(IServiceWrapper serviceWrapper)
+        {
+            _serviceWrapper = serviceWrapper;
+        }
+
+    }
+}
---
+++ GodPay-CMS/Controllers/PersonnelSetController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GodPay_CMS.Controllers
+{
+    /// <summary>
+    /// 特店人員管理頁面
+    /// </summary>
+    public class PersonnelSetController : Controller
+    {
+        /// <summary>
+        /// 特店人員首頁
+        /// </summary>
+        /// <returns></returns>
+        [ResponseCache(NoStore = true)]
+        [Authorize(Roles = "Store")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
