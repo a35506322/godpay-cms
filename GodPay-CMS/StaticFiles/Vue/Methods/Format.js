894365c3a8043941aa74bc48c4bcc0ba81542a23
莊理竣 <lijungjhuang@uitc.com.tw>
2022-01-18 14:22:04 +0800

    新增轉換訂單

--- GodPay-CMS/StaticFiles/Vue/Methods/Format.js
+++ GodPay-CMS/StaticFiles/Vue/Methods/Format.js
@@ -11,3 +11,25 @@ export function FormatTWDate(value) {
         return localDate;
     }
 }
+
+/**
+ * 轉換訂單狀態
+ * @param {any} value 狀態
+ */
+export function FormatOrderStatus(value) {
+    const statusEnum = Object.freeze({
+        1: '授權失敗',
+        2: '授權成功',
+        3: '取消',
+        4: '請款',
+        5: '退貨',
+        99: '錯誤單號'
+    })
+
+    if (value === null || value === '') {
+        return statusEnum['99']
+    }
+
+    return statusEnum[value]
+
+}
