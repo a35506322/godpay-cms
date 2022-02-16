a9d3dc942722653f673b450385849899184fea30
莊理竣 <lijungjhuang@uitc.com.tw>
2022-02-16 10:59:37 +0800

    將DataTable的Filter移至Mixins

--- GodPay-CMS/StaticFiles/Vue/Mixins/DataTableMixins.js
+++ GodPay-CMS/StaticFiles/Vue/Mixins/DataTableMixins.js
@@ -1,4 +1,6 @@
-﻿export default {
+﻿const { FilterMatchMode } = primevue.api;
+
+export default {
     components: {
         "p-datatable": primevue.datatable,
         "p-column": primevue.column,        
@@ -6,5 +8,20 @@
         "p-calendar": primevue.calendar,
         "p-scrollpanel": primevue.scrollpanel,
         "p-card": primevue.card
+    },
+    data: function () {
+        return {
+            filtersSearch: null,
+        }
+    },
+    created: function () {
+        this.InitFiltersSearch();
+    },
+    methods: {
+        InitFiltersSearch: function () {
+            this.filtersSearch = {
+                'global': { value: null, matchMode: FilterMatchMode.CONTAINS },
+            }
+        }
     }
 }
